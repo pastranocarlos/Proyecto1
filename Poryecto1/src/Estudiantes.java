6f20490d06f2f85e6679b607fedab31796fee44a
pastranocarlos <cpastrano@udlanet.ec>
2016-10-24 12:58:25 -0500

    Update Estudiantes.java

--- Poryecto1/src/Estudiantes.java
+++ Poryecto1/src/Estudiantes.java
@@ -3,11 +3,58 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package proyectoprogreso1;
 /**
  *
- * @author cisco
+ * @author user
  */
 public class Estudiantes {
     
+    private String nombre;
+    private String apellido;
+    private int matricula;
+    private int cedula;
+     public Estudiantes()
+    {
+        
+    }
+     
+    public Estudiantes(String nombre, String apellido, int matricula,int cedula) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.matricula = matricula;
+        this.cedula = cedula;
+       
+    }
+    
+   
+    
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public int getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+    public int getCedula() {
+        return cedula;
+    }
+    public void setCedula(int cedula) {
+        this.matricula = matricula;        
+    }
+    @Override
+     public String toString() {
+        return "Nombre: " + nombre + ", Apellido: " + apellido + ", Matricula: " + matricula + ", Cedula: " + cedula;
+     }
 }
