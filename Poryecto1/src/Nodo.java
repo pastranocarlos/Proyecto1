474e7590bad9c1cff1b226688ebd47bc618eb834
BryanTarco <bryan-9304@outlook.com>
2016-10-24 12:57:26 -0500

    Update Nodo.java

--- Poryecto1/src/Nodo.java
+++ Poryecto1/src/Nodo.java
@@ -3,11 +3,44 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package proyectoprogreso1;
+
 
 /**
  *
- * @author cisco
+ * @author user
  */
 public class Nodo {
     
+    private Estudiantes persona; 
+    private Nodo siguiente; 
+    
+      public Nodo(Estudiantes persona) {
+        this.persona = persona;
+        this.siguiente=null;
+    }
+
+    public Nodo(Estudiantes persona, Nodo siguiente) {
+        this.persona = persona;
+        this.siguiente = siguiente;
+    }
+
+    public Estudiantes getPersona() {
+        return persona;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setPersona(Estudiantes persona) {
+        this.persona = persona;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+      
+      
 }
+
